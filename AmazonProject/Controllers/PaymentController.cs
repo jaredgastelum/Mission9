37a12e6eb8c961fb2c8c24170b03079bda957838
Jared Gastelum <jaredgastelum@gmail.com>
2023-03-16 08:50:28 -0600

    checkout

---
+++ AmazonProject/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AmazonProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AmazonProject.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Checkout()
+        {
+            return View(new Payment());
+        }
+    }
+}
---
+++ AmazonProject/Models/Payment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AmazonProject.Models
+{
+    public class Payment
+    {
+        [Key]
+        [BindNever]
+        public int PaymentId { get; set; }
+
+        [BindNever]
+        public ICollection<BasketLineItem> Lines { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name:")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the first address line:")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+
+        [Required(ErrorMessage = "Please enter a city name:")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter a state:")]
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        [Required(ErrorMessage = "Please enter a country:")]
+        public string Country { get; set; }
+    }
+}
